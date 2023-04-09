f84b1c519940c7c692d6bf7182aea6ceceeb492b
Sudhanshu Dimri <hhsudhanshudimri@gmail.com>
2023-04-09 18:57:19 +0530

    changed choosing 2 players from list of 4 algorithm

--- app.js
+++ app.js
@@ -13,35 +13,35 @@ var players = [
     genBtn.addEventListener("click", makeTeam);
 }());
 
+function chooseNplayers(list, n = 2) {
+    // randomly sort list
+    const shuffled_list = list.slice().sort(() => Math.random() - 0.5);
+    // return first 'n' values
+    return shuffled_list.slice(0, n);
+}
+
+function remainingPlayers(list, t1_list) {
+    // empty list for team 2
+    const t2_list = [];
+    // select the players that are not there in team 1
+    for (const player of list) {
+        if (!t1_list.includes(player)) {
+            t2_list.push(player);
+        }
+    }
+    return t2_list;
+}
+
+function changeInnerHTML(teamDOM, list) {
+    teamDOM.innerHTML = `${list[0]} & ${list[1]}`;
+}
+
 function makeTeam() {
     team1.innerHTML = '';
     team2.innerHTML = '';
-    let playersCopy = [...players];
-    let length = players.length;
-    let count = 0;
-
-    while (length !== 0) {
-        let r = Math.floor(Math.random() * length);
-
-        switch (count) {
-            case 0: team1.innerHTML += `${playersCopy[r]} & `;
-                break;
-            case 1: team1.innerHTML += `${playersCopy[r]}`;
-                break;
-            case 2: team2.innerHTML += `${playersCopy[r]} & `;
-                break;
-            case 3: team2.innerHTML += `${playersCopy[r]}`;
-                break;
-        }
-
-        for (let i = 0; i < length; i++) {
-            if (i === r) {
-                playersCopy.splice(i, 1);
-            }
-        }
-
-        length--;
-        count++;
-    }
+    team1_players = chooseNplayers(players);
+    team2_players = remainingPlayers(players, team1_players);
+    changeInnerHTML(team1, team1_players);
+    changeInnerHTML(team2, team2_players);
 }
 
